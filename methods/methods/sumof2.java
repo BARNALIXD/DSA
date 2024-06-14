26fdf0ad44e736162b52f372d95a59aa45160476
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-06-14 14:55:16 +0000

    ..

--- methods/methods/sumof2.java
+++ methods/methods/sumof2.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class sumof2 {
     public static void main (String[] args) {
+        sum();
+    }
+    static void sum(){
         Scanner in = new Scanner(System.in);
+
 
         System.out.println("Enter a number 1 :");
         int num1 = in.nextInt();
@@ -12,5 +16,6 @@ public class sumof2 {
         int num2 = in.nextInt();
         int sum =  num1 +num2;
         System.out.println("the sum = " + sum);
+    
     }    
 }
