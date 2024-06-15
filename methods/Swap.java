a45facdfcc88acab6819db75a939c4bbb8244b3f
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-06-15 10:45:51 +0000

    .

---
+++ methods/Swap.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Swap the two numbers
+        int temp = a;
+        a = b;
+        b = temp;
+
+        // Print the swapped values
+        System.out.println("a: " + a + ", b: " + b);
+    }
+}
