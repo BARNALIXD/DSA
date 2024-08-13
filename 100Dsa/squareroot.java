18bf06e655886334e65d5f27c380eaddbfac7aef
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-08-13 01:07:08 +0000

    .

---
+++ 100Dsa/squareroot.java
@@ -0,0 +1,23 @@
+class Solution {
+    long floorSqrt(long n) {
+    
+        int count=0;
+        for(int i=1;i<=n/2+1;i++){
+            if(i*i>n){
+                count=i-1;
+                return count;
+            }
+            
+            else if(i*i==n){
+                count=i;
+                return count;
+            }
+            
+            
+            
+        }
+        
+        return count;
+        
+    }
+}
