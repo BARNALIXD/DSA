ce4279de46f46dbe9c2e0b99da924b269e724954
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-08-17 14:25:55 +0000

    .

---
+++ 100Dsa/productarraypuzzle.java
@@ -0,0 +1,23 @@
+
+
+// User function Template for Java
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        // code here
+        int n = nums.length;
+        long left[] = new long[n];
+        long right[] = new long[n];
+        left[0] = 1;
+        right[n-1] =1;
+        for(int i = 1; i < n ; i++){
+            left[i] = left[i-1]*nums[i-1];
+        }
+        for(int i = n-2 ; i >= 0 ; i--){
+            right[i] = right[i+1]*nums[i+1];
+        }
+        for(int i = 0; i < n ; i++){
+            left[i] = left[i]*right[i];
+        }
+        return left;
+    }
+}
