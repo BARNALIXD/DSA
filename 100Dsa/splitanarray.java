aa191c9c237b800603d1d1fa51c270f3606e07b1
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-08-18 04:54:17 +0000

    .

---
+++ 100Dsa/splitanarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canSplit(int arr[]) {
+        // code here
+        int sum2 = 0;
+        for(int i : arr){sum2+=i;}
+        int sum1 = 0;
+        for(int i : arr){
+            sum1 += i;
+            sum2 -= i;
+            if(sum1==sum2){return true;}
+        }
+        return false;
+    }
+}
