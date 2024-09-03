b3021f38855934ab93ec0ce313f545734be24c7b
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-03 20:08:01 +0000

    .

---
+++ 100Dsa/nth.java
@@ -0,0 +1,13 @@
+
+
+class Solution {
+    public long nthStair(int n) {
+        // Code here
+        int count=1;
+        while(n>1){
+            n-=2;
+            count++;
+        }
+        return count;
+    }
+}
