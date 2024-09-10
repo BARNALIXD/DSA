5f104a64434b9da5aba9a9b0c7548b46d334193d
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-10 05:29:37 +0000

    initialization

---
+++ 100Dsa/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+    public static void main(String args[]) {
+        String name = "Barnali";
+        String fullName = "Barnali Das";
+        String sentence = "My name is barnali";
+    }
+}
