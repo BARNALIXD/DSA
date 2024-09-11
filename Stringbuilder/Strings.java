a311230fc539cd5550a861d93bcc75abd3aaceab
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-11 09:47:35 +0000

    reversing the string using StringbUILDER

--- Stringbuilder/Strings.java
+++ Stringbuilder/Strings.java
@@ -3,12 +3,18 @@ import java.util.*;
 public class Strings {
 
     public static void main(String [] args) {
-        Stringbuilder sb = new Stringbuilder("h");
-        sb.append("e");
-        sb.append("l");
-        sb.append("l");
-        sb.append("o");
-       System.out.println(sb);
+        Stringbuilder sb = new Stringbuilder("hello");
        
+       for(int i = 0; i <sb.length()/2 ; i++){
+        int front = i;
+        int back = sb.length() -1 - i; //5-1-0
+
+        char frontChar = sb.charAt(front);
+        char backChar = sb.charAt(back);
+
+        sb.charAt(front, backChar);
+        sb.charAt(back, frontChar);
+       }
+       System.out.println(sb);
     }
 }
