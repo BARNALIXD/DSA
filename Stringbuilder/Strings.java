953340f438337a1eacd8ae9149919b0d644c2bba
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-11 09:15:29 +0000

    setCharAt

--- Stringbuilder/Strings.java
+++ Stringbuilder/Strings.java
@@ -5,5 +5,12 @@ public class Strings {
     public static void main(String [] args) {
         Stringbuilder sb = new Stringbuilder("Barnali);
         System.out.println(sb);
+
+        //charAt index 0
+        System.out.println(sb.charAt(0));
+
+        //set Character at index 0
+        sb.charAt(0,"p");
+        System.out.println(sb);
     }
 }
