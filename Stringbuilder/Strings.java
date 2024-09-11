cb0d4a7c8b65ef41eb12d321c62867097637af55
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-11 09:28:51 +0000

    appending

--- Stringbuilder/Strings.java
+++ Stringbuilder/Strings.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class Strings {
 
     public static void main(String [] args) {
-        Stringbuilder sb = new Stringbuilder("Barnali);
-        System.out.println(sb);
-        
-        sb.insert(0, "s');
-        System.out.println(sb);
-
-        //delete
-        sb.delete(2,3);
-        System.out.println(sb);
-        
+        Stringbuilder sb = new Stringbuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+       System.out.println(sb);
+       
     }
 }
