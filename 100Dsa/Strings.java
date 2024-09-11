7f0c5a73a3f21191397f5d243a84bb2911690a4f
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-11 05:54:57 +0000

    == method

--- 100Dsa/Strings.java
+++ 100Dsa/Strings.java
@@ -5,10 +5,19 @@ public class Strings {
       String name1 = "Tony";
       String name2 = "Tony";
 
-      if(name1.compareTo(name2)== 0) {
+      //if(name1.compareTo(name2)== 0) {
+       ////System.out.println("Strings are equal");
+      //}else {
+       // System.out.println("Strings are not equal");
+      //} 
+
+      
+      if(name1 == name2) {
        System.out.println("Strings are equal");
       }else {
         System.out.println("Strings are not equal");
       }
+
+     
     }
 }
