7ea0da1acf9231f46b9e00c77f0c30d1946fbb34
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-23 02:37:42 +0000

    1

---
+++ OOPS/oops.java
@@ -0,0 +1,29 @@
+public class oops{
+    public static void main (String[] args){
+        Pen p1 = new Pen();// created a pen object called p1
+        p1.setColor("pink");
+        System.out.println(p1.color);
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color  = newColor;
+    }
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class student {
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage (int phy, int chem , int bio){
+        percentage = (phy+chem+bio)/3;
+    }
+}
