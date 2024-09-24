e33f97135fb05e1f284782636a588e022ca16ccf
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-24 11:54:34 +0000

    getters and setters

---
+++ OOPS/oops2.java
@@ -0,0 +1,20 @@
+ class pen {
+    String color;
+    int tip;
+
+    String getColor(){
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+
+    void setTip(int tip){
+        this.tip = tip;
+    }
+}
