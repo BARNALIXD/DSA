2d546da4bd80fe1fba29392be32405bd66a6553a
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-25 13:03:38 +0000

    copy constructor

--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -1,14 +1,25 @@
 public class oops {
     public static void main(String [] args){
         Student s1 = new Student;
-        Student s2 = new Student("Barnali");
-        Student s3 = new student (123);
+        s1.name = "barnali";
+        s1.roll = 99;
+        s1.password = "abcd";
+
+        Student s2 = new Student(s1);
+        s2.password = "xys";
     }
 }
 
 class student {
     String name;
     int roll;
+    String password;
+
+//copy constructor
+    Student(Student s1){
+        this.name = s1.name;
+        this.roll = s1.roll;
+    }
 
     Student () {
         System.out.println("Constructor is called");
