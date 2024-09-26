09ab1bd0b284e76fb4b507e0af6d8c272d9247b8
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-26 14:48:59 +0000

    .

--- OOPS/shalow.java
+++ OOPS/shalow.java
@@ -12,3 +12,19 @@ public class Ex {
 		System.out.println( Arrays.toString(data) ); 
 	} 
 } 
+
+
+public class UsesEx{ 
+
+	public static void main(String[] args) { 
+		int[] vals = {3, 7, 9}; 
+		Ex e = new Ex(vals); 
+		e.showData(); // prints out [3, 7, 9] 
+		vals[0] = 13; 
+		e.showData(); // prints out [13, 7, 9] 
+
+		// Very confusing, because we didn't 
+		// intentionally change anything about 
+		// the object e refers to. 
+	} 
+} 
