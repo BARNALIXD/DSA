ab17f608fdf7133ddf167892b7e756accad54afc
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-26 14:48:28 +0000

    .

---
+++ OOPS/shalow.java
@@ -0,0 +1,14 @@
+//code illustrating shallow copy 
+public class Ex { 
+
+	private int[] data; 
+
+	// makes a shallow copy of values 
+	public Ex(int[] values) { 
+		data = values; 
+	} 
+
+	public void showData() { 
+		System.out.println( Arrays.toString(data) ); 
+	} 
+} 
