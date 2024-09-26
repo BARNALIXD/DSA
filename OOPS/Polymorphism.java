709e80c92d25455ce46e01e9ede0e97c0a1bdd31
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-09-26 13:46:44 +0000

    deep copy constructor

--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -25,16 +25,21 @@ class student {
     int marks[];
 
 //Shallow copy constructor
-    Student(Student s1){
-        marks = new int[3];
-        this.name = s1.name;
-        this.roll = s1.roll;
-        this.marks = s1.marks;
-    }
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks = s1.marks;
+    // }
 
     //deep copy constructor
     Student (Student s1) {
-        
+         marks = new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        for (int i = 0 ; i<marks.length ; i++){
+            this.marks[i] = s1.marks[i];
+        }
     }
 
     Student () {
