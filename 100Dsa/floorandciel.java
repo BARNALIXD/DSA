e5302c6fa4af298127c315681bbe57b8236bb536
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-10-15 14:09:09 +0000

    .

--- 100Dsa/floorandciel.java
+++ 100Dsa/floorandciel.java
@@ -1,35 +1,3 @@
-class Solution
-{
-	// Function to find the peak element
-	// arr[]: input array
-	// n: size of array a[]
-	public int peakElement(int[] arr,int n)
-    {
-       int low = 0;
-       int high = arr.length - 1 ;
-       
-       while(low <= high) {
-           int mid = (high+low) / 2;
-           
-           if((mid == 0 || arr[mid] >= arr[mid-1])
-           && (mid == arr.length - 1 || arr[mid] >= arr[mid +1])) {
-               return mid;
-           }else if(mid> 0 && arr[mid - 1] >= arr[mid]){
-               high = mid -1 ;
-           }else{
-               low = mid +1 ;
-           }
-       }
-       return -1;
-    }
-}
-//
-
-
-
-
-//2nd bruteforce
-
 import java.util.*;
 
 public class tUf {
