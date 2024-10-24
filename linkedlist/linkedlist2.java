49ecbbfc9e2dece9e939e5c23bee2807bd8a7df4
BARNALIDAS <dasbarnali690@gcekbpatna.ac.in>
2024-10-24 14:59:08 +0000

    .

---
+++ linkedlist/linkedlist2.java
@@ -0,0 +1,25 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    public Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+public class Main{
+    public static main (String args[]) {
+        int[] arr = {2,5,8,7};
+
+        Node x = new Node(arr[0]);
+
+        Node y = x;
+
+        System.o
+    }
+}
